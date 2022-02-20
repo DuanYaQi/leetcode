f81b1feff0ee8fd2c54436c061693b3995b5a767
段雅琪 <837738300@qq.com>
2022-02-20 20:49:58 +0800

    update

--- 94+.cpp
+++ 94+.cpp
@@ -14,21 +14,14 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         stack<TreeNode*> st;
         vector<int> res;
-        if (root != nullptr) st.push(root);        
+        if (root == nullptr) return res;
+        st.push(root);        
         while(!st.empty()) {
             TreeNode* node = st.top();
-            if (node != nullptr) {
-                st.pop();
-                if (node->right != nullptr) st.push(node->right);
-                st.push(node);
-                st.push(nullptr);
-                if (node->left != nullptr) st.push(node->left);
-            } else {
-                st.pop();
-                node = st.top();
-                st.pop();
-                res.push_back(node->val);
-            }
+            st.pop();
+            res.push_back(node->val);
+            if (node->right != nullptr) st.push(node->right);
+            if (node->left != nullptr) st.push(node->left);
         }
         return res;
     }
