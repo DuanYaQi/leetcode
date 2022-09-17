6b14f37ea74c5b34ad6a9f06a484d0667865d6cb
段雅琪 <837738300@qq.com>
2022-09-17 13:28:29 +0800

    add heap

--- Alog_Sort/heapSort.cpp
+++ Alog_Sort/heapSort.cpp
@@ -1,74 +1,49 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-const int maxn = 10;
+const int maxn=100;
+//heap为堆,n为元素个数
+int heap[maxn], n=10;
 
-int heap[maxn], n = 9;
-
-// 对heap数组在[low, high] 范围进行向下调整
-// low 为欲调整结点的数组下标，high一般为堆的最后一个元素的数组下标
-
-void downAdjust(int lo, int hi) {
-    int i = lo, j = i * 2;  // i 为欲调整结点，j为其左孩子
-    while (j <= hi) {   // 存在孩子结点
+// 对heap数组在[low, high]范围进行向下调整   O(logN)
+void downAdjust(int lo, int hi){
+    int i = lo, j = i * 2;  // i 为欲调整结点，j 为其左孩子
+    while (j <= hi) {
         if (j + 1 <= hi && heap[j + 1] > heap[j]) {
             j++;
         }
 
         if (heap[j] > heap[i]) {
-            swap(heap[j], heap[i]);
+            swap(heap[i], heap[j]);
             i = j;
             j = i * 2;
-        } else {
-            break;
-        }
+        } else break;
     }
 }
 
-void upAdjust(int lo, int hi) {
-    int i = hi, j = i / 2;
-
-    while (j >= lo) {
-        if (heap[j] < heap[i]) {
-            swap(heap[j], heap[i]);
-            i = j;
-            j = i / 2;
-        } else {
-            break;
-        }
-    }
-}
-
-//删除堆顶元素 O(logN)
-void deleteTop(){
-    heap[1] = heap[n--];
-    downAdjust(1, n);
-}
-
-
-void insert(int x){
-    heap[++n] = x;	//元素个数+1
-    upAdjust(1, n);	//向上调整新加入的结点 n
-}
-
-
-//建堆  O(N)
-void createHeap(){
-    for (int i = n / 2; i >= 1; i--){
+void createHeap() {
+    for (int i = n / 2; i >= 1; i--) {
         downAdjust(i, n);
     }
 }
 
-int main() {
-    for (int i = 1; i <= n; ++i) {
-        heap[i] = rand() % 10;
-    }
+void heapSort() {
     createHeap();
-    deleteTop();
-    insert(rand() % 5);
+    // 排序过程
+    for (int i = n; i > 1; --i) {
+        swap(heap[i], heap[1]);
+        downAdjust(1, i - 1);
+    }
+}
 
+int main () {
+    for (int i = 1; i <= n; ++i) {
+        int r = rand() % 10;
+        heap[i] = r;
+    } 
+
+    heapSort();
     return 0;
 }
